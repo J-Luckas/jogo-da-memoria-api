554032289371bb5a3dcdeefe99e58c72beca6e71
Jairo Luckas <jairoluckas@hotmail.com>
2023-01-04 23:23:55 -0300

    feat: start

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('/server/data.json');
+const router = jsonServer.router('./server/data.json');
 const middlewares = jsonServer.defaults();
 const port = 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => console.log('listening on port 3000'));
